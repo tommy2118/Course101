45e4305a8457836e1be5f88c56435c1603421326
Tommy A. Caruso <tommy.caruso2118@gmail.com>
2016-05-14 09:46:34 -0400

    Complete Medium 3

---
+++ Lesson3/exercises_medium_3.rb
@@ -0,0 +1,26 @@
+# Exercises - Medium 3
+# Exercise #1
+puts "\nExercise #1"
+
+
+# Exercise #2
+puts "\nExercise #2"
+
+# Exercise #3
+puts "\nExercise #3"
+
+puts "My string looks like this now: pumkins"
+puts "My array looks like this now: ["pumpkins", "rutabaga"]
+
+# Exercise #4
+puts "\nExercise #4"
+
+puts "My string looks like this now: rutabaga"
+puts "My array looks like this now: ["pumpkins"]"
+
+# Exercise #5
+puts "\nExercise #5"
+
+def color_valid(color)
+  color == "blue" || color == "green"
+end
