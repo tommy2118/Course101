bd398496ebac69a5c0e69171cefee35a6e6ee06d
Tommy A. Caruso <tommy.caruso2118@gmail.com>
2016-05-22 23:20:26 -0400

    Complete 21 game

--- Lesson4/21_card_game.rb
+++ Lesson4/21_card_game.rb
@@ -21,30 +21,16 @@ end
 def display_dealers_initial_hand(card_one, _card_two)
   puts ""
   puts "+" + "-" * 11 + "+" + "  " + "+" + "-" * 11 + "+\n" \
-    + "|" + card_one[0].to_s + " " * 10 + "|" + "  " + "|" + " " * 11 + "|\n" \
+    + "|" + card_one[0].to_s + " " * (card_one[0] == 10 ? 9 : 10) + "|" \
+    + "  " + "|" + " " * 11 + "|\n" \
     + "|" + " " * 11 + "|" + "  " + "|" + " " * 11 + "|\n" \
     + "|" + " " * 11 + "|" + "  " + "|" + " " * 11 + "|\n" \
     + "|" + " " * 5 + card_one[1] + " " * 5 + "|" + "  " + "|" + " " * 11 \
     + "|\n" + "|" + " " * 11 + "|" + "  " + "|" + " " * 11 + "|\n" \
     + "|" + " " * 11 + "|" + "  " + "|" + " " * 11 + "|\n" \
-    + "|" + " " * 10 + card_one[0].to_s + "|" + "  " + "|" + " " * 11 + "|\n" \
+    + "|" + " " * (card_one[0] == 10 ? 9 : 10) + card_one[0].to_s + "|" + "  " \
+    + "|" + " " * 11 + "|\n" \
     + "+" + "-" * 11 + "+" + "  " + "+" + "-" * 11 + "+"
-end
-
-def display_two_card_hand(card_one, card_two)
-  puts ""
-  puts "+" + "-" * 11 + "+" + "  " + "+" + "-" * 11 + "+"
-  puts "|" + card_one[0].to_s + " " * 10 + "|" + "  " + "|" + \
-       card_two[0].to_s + " " * 10 + "|"
-  puts "|" + " " * 11 + "|" + "  " + "|" + " " * 11 + "|"
-  puts "|" + " " * 11 + "|" + "  " + "|" + " " * 11 + "|"
-  puts "|" + " " * 5 + card_one[1] + " " * 5 + "|" + "  " + "|" + " " * 5 \
-       + card_one[1] + " " * 5 + "|"
-  puts "|" + " " * 11 + "|" + "  " + "|" + " " * 11 + "|"
-  puts "|" + " " * 11 + "|" + "  " + "|" + " " * 11 + "|"
-  puts "|" + " " * 10 + card_one[0].to_s + "|" + "  " + "|" + " " * 10 + \
-       card_two[0].to_s + "|"
-  puts "+" + "-" * 11 + "+" + "  " + "+" + "-" * 11 + "+"
 end
 
 def render_cards(*hand)
@@ -92,40 +78,112 @@ end
 def total_cards(hand)
   total = 0
   hand.each do |card|
-    total += if card[0] == "J" || card[0] == "Q" || card[0] == "K"
-               10
-             elsif
-               card[0] == "A"
+    total += if card[0] == "A"
                11
+             elsif card[0].to_i == 0
+               10
              else
                card[0]
              end
   end
-  total -= 10 if total >= 21 && hand.flatten.include?("A")
+  hand.flatten.select { |value| value == "A" }.count.times do
+    total -= 10 if total > 21
+  end
+
   total
 end
 
 def busted?(hand)
-  total_cards(hand) > 21
+  return true if total_cards(hand) > 21
 end
 
-players_hand = []
-dealers_hand = []
-deck = initialize_deck
-deal_card(deck, players_hand)
-deal_card(deck, dealers_hand)
-deal_card(deck, players_hand)
-deal_card(deck, dealers_hand)
-display_dealers_initial_hand(*dealers_hand)
-render_cards(*players_hand)
-puts total_cards(players_hand)
-puts busted?(players_hand)
-loop do
-  prompt("Type hold to stay")
-  players_move = gets.chomp
-  break if busted?(players_hand) || players_move == "hold"
-  system('clear')
-render_cards(*players_hand)
-puts total_cards(players_hand)
-puts busted?(players_hand)
+def display_board_players_turn(players_hand, dealers_hand)
+  system('clear') || system('cls')
+  display_dealers_initial_hand(*dealers_hand)
+  render_cards(*players_hand)
+  puts "Player: #{total_cards(players_hand)}"
 end
+
+def display_board_dealers_turn(players_hand, dealers_hand)
+  system('clear')
+  render_cards(*dealers_hand)
+  render_cards(*players_hand)
+  puts "Player: #{total_cards(players_hand)}"
+  puts "Dealer: #{total_cards(dealers_hand)}"
+end
+
+def set_up_game(deck, players_hand, dealers_hand)
+  deal_card(deck, players_hand)
+  deal_card(deck, dealers_hand)
+  deal_card(deck, players_hand)
+  deal_card(deck, dealers_hand)
+  display_board_players_turn(players_hand, dealers_hand)
+end
+
+loop do
+  players_hand = []
+  dealers_hand = []
+  players_answer = ''
+  deck = initialize_deck
+  set_up_game(deck, players_hand, dealers_hand)
+  loop do
+    loop do
+      loop do
+        prompt("Type 'h' to hit or 's' to stay")
+        players_answer = gets.chomp.downcase
+        break if players_answer == 'h' || players_answer == 's'
+        prompt("please enter a valid selection.")
+      end
+
+      if busted?(players_hand)
+        display_board_players_turn(players_hand, dealers_hand)
+        puts "You busted! Game over!"
+        break
+      elsif players_answer == 's'
+        display_board_players_turn(players_hand, dealers_hand)
+        puts "You chose to stay!"
+        break
+      else
+        deal_card(deck, players_hand)
+        display_board_players_turn(players_hand, dealers_hand)
+        if busted?(players_hand)
+          display_board_players_turn(players_hand, dealers_hand)
+          puts "You busted! Game over!"
+          break
+        end
+      end
+    end
+
+    break if busted?(players_hand)
+
+    loop do
+      display_board_dealers_turn(players_hand, dealers_hand)
+      sleep(1)
+      deal_card(deck, dealers_hand) if total_cards(dealers_hand) < 17
+      display_board_dealers_turn(players_hand, dealers_hand)
+      break if busted?(dealers_hand) || total_cards(dealers_hand) >= 17
+    end
+
+    break
+  end
+
+  if busted?(dealers_hand) || total_cards(dealers_hand) < \
+    total_cards(players_hand) && !busted?(players_hand)
+    puts "Player wins!"
+  elsif total_cards(players_hand) == total_cards(dealers_hand)
+    puts "The hand is a push!"
+  else
+    puts "Dealer wins!"
+  end
+
+  answer = ''
+  loop do
+    prompt("Would you like to play again? 'y' or 'n'?")
+    answer = gets.chomp.downcase
+    break if answer == 'y' || answer == 'n'
+    puts "Please enter a valid response"
+  end
+
+  break if answer == "n"
+end
+puts "Thanks for playing!"
